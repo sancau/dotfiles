602e66150c6146bec49a6893b8ba28e0bb7c012e
Alexander Tatchin <alexander.tatchin@gmail.com>
2020-04-18 18:02:17 +0300

    Update init.sh for Manjaro i3.

--- init.sh
+++ init.sh
@@ -8,4 +8,6 @@ setxkbmap -layout us,ru
 setxkbmap -option 'grp:win_space_toggle'
 xset r rate 200 50
 sudo nmcli c u VPN
+sudo mount /dev/nvme0n1p2 /home/sancau/win_hdd/
 bash ~/.screenlayout/2_dell.sh
+alias cs='import png:- | xclip -selection clipboard -t image/png'
