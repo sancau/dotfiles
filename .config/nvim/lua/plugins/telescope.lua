529aa4b2475327490bdc52fa72de41b2c2c99c5e
Alexander Tatchin <alexander.tatchin@gmail.com>
2024-03-15 17:57:49 +0300

    nvim. telescope. hidden files.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,21 +4,36 @@ return {
         tag = '0.1.6',
         dependencies = { 'nvim-lua/plenary.nvim' },
         config = function()
-            -- local telescope = require('telescope')
+            local telescope = require('telescope')
             -- local actions = require('telescope.actions')
-            -- telescope.setup({
-            --     defaults = {
-            --         mappings = {
-            --             i = {
-            --                 ["<esc>"] = actions.close,
-            --                 ["<C-j>"] = actions.move_selection_next,
-            --                 ["<C-k>"] = actions.move_selection_previous
-            --             },
-            --         },
-            --     },
-            -- })
+            telescope.setup({
+                defaults = {
+                    -- mappings = {
+                    --     i = {
+                    --         ["<esc>"] = actions.close,
+                    --         ["<C-j>"] = actions.move_selection_next,
+                    --         ["<C-k>"] = actions.move_selection_previous
+                    --     },
+                    -- },
+                    file_ignore_patterns = {
+                        -- 'node_modules/',
+                        -- 'venv/',
+                        -- '.venv/',
+                        '__pycache__/',
+                        '.ipynb_checkpoints/',
+                        '.git/'
+                    }
+                },
+                pickers = {
+                    live_grep = {
+                        additional_args = function(opts)
+                            return {"--hidden"}
+                        end
+                    },
+                },
+            })
             local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>f', function() builtin.find_files({hidden = true}) end, {})
             vim.keymap.set('n', '<leader>s', builtin.live_grep, {})
         end,
     },
