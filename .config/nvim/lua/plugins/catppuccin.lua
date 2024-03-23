4308aa5fb62051f2e0ba55dc4459cbd5cf47f0a5
Alexander Tatchin <alexander.tatchin@gmail.com>
2024-03-23 18:47:49 +0300

    nvim. tinker some catppuccin colors for trouble plugin.

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,9 +1,21 @@
+local vim = vim
+
 return {
     'catppuccin/nvim',
     lazy = false,
     name = 'catppuccin',
     priority = 1000,
     config = function()
+        local catppuccin = require("catppuccin")
+        catppuccin.setup({
+            custom_highlights = function(c)
+                return {
+                    TroubleText = { fg = c.text },
+                    TroubleCount = { fg = c.yellow },
+                    -- TroubleNormal = { fg = c.fg, bg = c.bg_sidebar },
+                }
+            end
+        })
         vim.cmd.colorscheme "catppuccin"
     end
 }
