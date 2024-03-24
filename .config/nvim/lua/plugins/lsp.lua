557925bd788654349187efa968000a31ecb1b932
Alexander Tatchin <alexander.tatchin@gmail.com>
2024-03-24 22:58:10 +0300

    nvim. ruff-lsp. format on save, sort imports on save.

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,9 +1,9 @@
 local vim = vim
 
 local has_words_before = function()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 local servers = { "lua_ls", "pyright" }
@@ -24,6 +24,9 @@ return {
         end
     },
     {
+        'astral-sh/ruff-lsp',
+    },
+    {
         'microsoft/python-type-stubs',
     },
     {
@@ -32,33 +35,74 @@ return {
             local lspconfig = require("lspconfig")
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            for _, lsp in ipairs(servers) do
-              lspconfig[lsp].setup {
-               settings = {
-                    pyright = {
-                            autoImportCompletion = true,
-                    },
-                    python = {
-                        analysis = {
-                            autoSearchPaths = true,
-                            diagnosticMode = 'openFilesOnly',
-                            useLibraryCodeForTypes = true,
-                            typeCheckingMode = 'off',
-                            stubPath = vim.fn.stdpath("data") .. "/lazy/python-type-stubs",
-                        }
+            lspconfig.ruff_lsp.setup({
+                init_options = {
+                    settings = {
+                        -- Any extra CLI arguments for `ruff` go here.
+                        args = {},
                     }
-                },
-               on_attach = function(_, bufnr)
-               require "lsp_signature".on_attach({
-                 bind = true, -- This is mandatory, otherwise border config won't get registered.
-                 hint_enable = false,
-                 floating_window = false,
-                 toggle_key_flip_floatwin_setting = false,  -- persistent floating window toggle
-                 doc_lines = 25,
-               }, bufnr)
-               end,
-                capabilities = capabilities,
-              }
+                }
+            })
+
+            local on_attach = function(client, bufnr)
+                if client.name == 'ruff_lsp' then
+                    -- Disable hover in favor of Pyright
+                    client.server_capabilities.hoverProvider = false
+                end
+
+                local opts = {}
+                vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+                vim.keymap.set('n', '<space>f', function()
+                    vim.lsp.buf.format { async = true }
+                end, opts)
+
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format()
+                            vim.lsp.buf.code_action({
+                                context = { only = { "source.organizeImports" } },
+                                apply = true,
+                            })
+                            vim.wait(100)
+                        end,
+                    })
+                end
+            end
+
+            require('lspconfig').ruff_lsp.setup {
+                on_attach = on_attach,
+            }
+
+
+            for _, lsp in ipairs(servers) do
+                lspconfig[lsp].setup {
+                    settings = {
+                        pyright = {
+                            -- Using Ruff's import organizer
+                            disableOrganizeImports = true,
+                        },
+                        python = {
+                            analysis = {
+                                -- Ignore all files for analysis to exclusively use Ruff for linting
+                                ignore = { '*' },
+                            },
+                        },
+                    },
+                    on_attach = function(_, bufnr)
+                        require "lsp_signature".on_attach({
+                            bind = true, -- This is mandatory, otherwise border config won't get registered.
+                            hint_enable = false,
+                            floating_window = false,
+                            toggle_key_flip_floatwin_setting = false, -- persistent floating window toggle
+                            doc_lines = 25,
+                        }, bufnr)
+                    end,
+                    capabilities = capabilities,
+                }
             end
 
             local opts = {}
@@ -66,6 +110,7 @@ return {
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
             vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
             vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+            vim.keymap.set('n', '<space>r', vim.lsp.buf.rename, opts)
         end
     },
     {
@@ -74,49 +119,49 @@ return {
             local cmp = require("cmp")
             local luasnip = require("luasnip")
             cmp.setup {
-              completion = {
-                autocomplete = false
-              },
-              snippet = {
-                expand = function(args)
-                  luasnip.lsp_expand(args.body)
-                end,
-              },
-              mapping = cmp.mapping.preset.insert({
-                ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
-                ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
-                -- C-b (back) C-f (forward) for snippet placeholder navigation.
-                ['<C-Space>'] = cmp.mapping.complete(),
-                -- Make the TAB to work for "from a import " cases
-                ['<CR>'] = cmp.mapping.confirm {
-                  behavior = cmp.ConfirmBehavior.Replace,
-                  select = true,
+                completion = {
+                    autocomplete = false
                 },
-                ['<Tab>'] = cmp.mapping(function(fallback)
-                  if cmp.visible() then
-                    cmp.select_next_item()
-                  elseif luasnip.expand_or_jumpable() then
-                    luasnip.expand_or_jump()
-                  elseif has_words_before() then
-                    cmp.complete()
-                  else
-                    fallback()
-                  end
-                end, { 'i', 's' }),
-                ['<S-Tab>'] = cmp.mapping(function(fallback)
-                  if cmp.visible() then
-                    cmp.select_prev_item()
-                  elseif luasnip.jumpable(-1) then
-                    luasnip.jump(-1)
-                  else
-                    fallback()
-                  end
-                end, { 'i', 's' }),
-              }),
-              sources = {
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' },
-              },
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    -- ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+                    -- ['<C-d>'] = cmp.mapping.scroll_docs(4),  -- Down
+                    -- C-b (back) C-f (forward) for snippet placeholder navigation.
+                    -- ['<C-Space>'] = cmp.mapping.complete(),
+                    -- Make the TAB to work for "from a import " cases
+                    ['<CR>'] = cmp.mapping.confirm {
+                        behavior = cmp.ConfirmBehavior.Replace,
+                        select = true,
+                    },
+                    ['<Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        elseif has_words_before() then
+                            cmp.complete()
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                    ['<S-Tab>'] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { 'i', 's' }),
+                }),
+                sources = {
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                },
             }
         end
     },
@@ -128,7 +173,8 @@ return {
     {
         "ray-x/lsp_signature.nvim",
         config = function()
-            vim.keymap.set({'i', 'n'}, '<C-w>', require('lsp_signature').toggle_float_win, { silent = true, noremap = true, desc = 'Toggle signature' })
+            vim.keymap.set({ 'i', 'n' }, '<C-w>', require('lsp_signature').toggle_float_win,
+                { silent = true, noremap = true, desc = 'Toggle signature' })
         end
     },
     {
@@ -142,5 +188,5 @@ return {
         "saadparwaiz1/cmp_luasnip",
         config = function()
         end
-    },
+    }
 }
