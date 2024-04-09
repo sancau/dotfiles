854d6b2b950515caf5334bbb6aeaad687adf2e55
Alexander Tatchin <alexander.tatchin@gmail.com>
2024-04-10 00:01:06 +0300

    install script for WSL machine.

---
+++ install.sh
@@ -0,0 +1,102 @@
+sudo apt-get update && sudo apt-get upgrade -y
+
+# docker install
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# WSL docker sock fix
+sudo update-alternatives --config iptables  # choose option 1
+
+sudo usermod -aG docker ${USER}
+sudo service docker start
+
+# base dependencies
+sudo apt-get update &&\
+    sudo apt-get upgrade -y &&\
+    DEBIAN_FRONTEND=noninteractive sudo apt-get install -y \
+        curl \
+        git \
+        stow \
+        ripgrep \
+        wget \
+        zip \
+        htop \
+        bat \
+        build-essential \
+        libssl-dev \
+        zlib1g-dev \
+        libbz2-dev \
+        libreadline-dev \
+        libsqlite3-dev \
+        libncursesw5-dev \
+        xz-utils \
+        tk-dev \
+        libxml2-dev \
+        libxmlsec1-dev \
+        libffi-dev \
+        liblzma-dev \
+        locales \
+        pipx \
+        python3.11-venv \
+        python3-tk \
+        tk-dev &&\
+    mkdir -p $HOME/.local/bin &&\
+    ln -s /usr/bin/batcat $HOME/.local/bin/bat &&\
+    pipx ensurepath
+
+# Zsh
+sudo apt-get install -y zsh &&\
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &&\
+    chsh -s $(which zsh) &&\
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git /root/.oh-my-zsh/custom/plugins/zsh-autosuggestions &&\
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /root/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting &&\
+    git clone https://github.com/MichaelAquilina/zsh-you-should-use.git /root/.oh-my-zsh/custom/plugins/you-should-use &&\
+    git clone https://github.com/fdellwing/zsh-bat.git /root/.oh-my-zsh/custom/plugins/zsh-bat
+
+# Zoxied
+curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+
+# Tmux
+sudo apt-get install -y tmux &&\
+    git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
+
+# PyEnv
+curl https://pyenv.run | bash
+
+# Install global Python (versin specified explicitly)
+export PYENV_ROOT="$HOME/.pyenv" &&\
+    export GLOBAL_PYTHON_VERSION=3.10 &&\
+    [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH" &&\
+    eval "$(pyenv init -)" &&\
+    eval "$(pyenv virtualenv-init -)" &&\
+    pyenv install ${GLOBAL_PYTHON_VERSION} &&\
+    pyenv global ${GLOBAL_PYTHON_VERSION}
+
+# Install NodeJS
+curl -fsSL https://deb.nodesource.com/setup_21.x | sudo bash && sudo apt-get install nodejs -y
+
+# Neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz  &&\
+    rm -rf /opt/nvim &&\
+    sudo tar -C /opt -xzf nvim-linux64.tar.gz
+
+# Install global Poetry
+pipx ensurepath &&\
+    pipx install poetry &&\
+    mkdir $HOME/.zfunc &&\
+    $HOME/.local/bin/poetry completions zsh > $HOME/.zfunc/_poetry
+
+# LINK DOTFILES THEN
+#
+## Tmux plugins
+# bash $HOME/.tmux/plugins/tpm/scripts/install_plugins.sh
+
+
