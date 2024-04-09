45594962e65f177615f7131405ec3f506e05d495
Alexander Tatchin <alexander.tatchin@gmail.com>
2024-04-09 17:26:58 +0300

    python tools debug.

--- sandbox/main.py
+++ sandbox/main.py
@@ -1,6 +1,10 @@
-def bar():
-    pass
-
-
 def foo(value):
     return value**2
+
+
+class Controller:
+    def __init__(self, a):
+        self.a = a
+
+    def test(self):
+        return self.a
--- sandbox/test_main.py
+++ sandbox/test_main.py
@@ -1,5 +1,11 @@
-from main import bar, foo
+from main import Controller, foo
 
 
 def test_nothing():
     assert foo(2) == 4
+
+
+def test_controller():
+    c = Controller(42)
+
+    assert c.test() == 42
