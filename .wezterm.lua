60f16ca867399f1d854767228e25b0e16c9fcc06
Alexander Tatchin <alexander.tatchin@gmail.com>
2024-04-11 19:46:41 +0300

    wezterm config. default command.

--- .wezterm.lua
+++ .wezterm.lua
@@ -35,9 +35,12 @@ local THEME_ALABASTER = {
 
 -- config.color = THEME_ALABASTER
 
+
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.audible_bell = "Disabled"
 config.color_scheme = "Catppuccin Mocha"
+
+config.default_prog = { 'wsl', '~', '-d', 'WS', '-u', 'sancau' }
 
 return config
