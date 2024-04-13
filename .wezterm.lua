dd152180ac47ec035028508eed4dde2b9a3ac75c
Alexander Tatchin <alexander.tatchin@gmail.com>
2024-04-13 14:21:58 +0300

    wezterm.lua, start in fullscreen

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,11 @@
 local wezterm = require 'wezterm'
-
 local config = {}
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function()
+  local tab, pane, window = mux.spawn_window{}
+  window:gui_window():toggle_fullscreen()
+end)
 
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
@@ -8,7 +13,6 @@ config.audible_bell = "Disabled"
 config.color_scheme = "Catppuccin Mocha"
 
 config.default_prog = { 'wsl', '~', '-d', 'WS', '-u', 'sancau' }
-config.window_close_confirmation = "NeverPrompt"
+config.window_close_confirmation = 'NeverPrompt'
 
 return config
-
